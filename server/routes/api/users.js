7dad515acc4fef78eb8d5d9f8576d741641be523
gjsimkins <baseball3gary@gmail.com>
2021-12-17 16:35:14 -0800

    Implemented register users on server

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -18,15 +18,28 @@ router.route("/register")
                 password: req.body.password
             });
             // generate token
+            const token = user.generateToken();
             const doc = await user.save();
             // send email (at end of course)
             // save token and send with cookie
-            res.cookie('access-token', 'TODO')
-                .status(200).send(doc);
+            res.cookie('access-token', token)
+                .status(200).send(getUserProps(doc));
         } catch (error) {
             res.status(400).json({ message: 'Error', error: error })
         }
     })
+
+const getUserProps = (user) => {
+    return {
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        role: user.role
+
+    }
+}
 
 
 module.exports = router;
